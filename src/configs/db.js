48053692591aef66931347e7a5fb02c4fc5292ea
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2022-04-02 20:52:20 +0530

    database updated

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://vikash:spiderman@cluster0.bxhyu.mongodb.net/alDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://vikash:spiderman@cluster0.bxhyu.mongodb.net/lDatabase?retryWrites=true&w=majority"
   );
 }
