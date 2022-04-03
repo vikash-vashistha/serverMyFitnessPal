e35919bab631ec3f1ed242e9b8df72285982cca8
Vikash Vashistha <vikashvashistha.dev@gmail.com>
2022-04-03 07:41:03 +0530

    db changes

--- src/configs/db.js
+++ src/configs/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = () => {
   return mongoose.connect(
-    "mongodb+srv://vikash:spiderman@cluster0.bxhyu.mongodb.net/lDatabase?retryWrites=true&w=majority"
+    "mongodb+srv://skale0181:sagar@cluster0.wqpjq.mongodb.net/myfitnesspaldata"
   );
 }
